5f0280ddea3bbfaef3b6935602972d1217b26653
Chvrls <charlesmabunga@gmail.com>
2024-05-03 21:28:09 +0800

    removing null using array filter

--- practice.js
+++ practice.js
@@ -37,13 +37,24 @@ const myArray = [
   func();
   */
 
-  const arr = ['safdsadfsdfg', 'dsfsdvsdv', 'fdssdbfddaf', 'gdfgdvd', 20, 20, 40, 50, 60, 'dsadasf', 'dfsvsv'];
+  const arr = ['safdsadfsdfg', 'dsfsdvsdv', 'fdssdbfddaf', 'gdfgdvd', 20, 20, 40, 50, 60, 'dsadasf', 'dfsvsv', null, null];
 
   function functionForArr () {
     for (let i = 0; i < arr.length; i++) {
       let output = arr[i];
       console.log(output);
     }
+    removeNull();
   };
-
   functionForArr();
+
+
+  function removeNull () {
+  let arrFiltered = arr.filter ((el) => {
+    return el != null;
+  });
+
+  console.log(arrFiltered);
+};
+
+removeNull();
